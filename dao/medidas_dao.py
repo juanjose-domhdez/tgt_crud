c39d10e49bdad044c812d5f68b4e1627f3b48239
Jimmy Moises Perez Fernandez <pfmoy29@gmail.com>
2026-07-30 22:27:18 -0600

    tablass bien

---
+++ dao/medidas_dao.py
@@ -0,0 +1,19 @@
+class Medida:
+
+    def __init__(
+        self,
+        id_medida=None,
+        id_pedido=None,
+        pecho=0.0,
+        cintura=0.0,
+        hombros=0.0,
+        manga=0.0,
+        largo_pantalon=0.0,
+    ):
+        self.id_medida = id_medida
+        self.id_pedido = id_pedido
+        self.pecho = pecho
+        self.cintura = cintura
+        self.hombros = hombros
+        self.manga = manga
+        self.largo_pantalon = largo_pantalon
